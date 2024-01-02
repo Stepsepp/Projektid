45ba33dcc2838c732e53cbee0a17777f5d1ca50c
RihoSepp <126762252+Stepsepp@users.noreply.github.com>
2024-01-02 18:02:01 +0200

    Update Create a Password Generator.py

--- Create a Password Generator.py
+++ Create a Password Generator.py
@@ -1,53 +1,32 @@
-# Password Generator Project
-# Antud olukorras tuleb siiski ette anda mis tähti me tahame, numbreid ja sümboleid, selle järgi hakkab tööle ka import random,
-
 import random
+
 letters = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z', 'A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'J', 'K', 'L', 'M', 'N', 'O', 'P', 'Q', 'R', 'S', 'T', 'U', 'V', 'W', 'X', 'Y', 'Z']
 numbers = ['0', '1', '2', '3', '4', '5', '6', '7', '8', '9']
 symbols = ['!', '#', '$', '%', '&', '(', ')', '*', '+']
 
 print("Welcome to the PyPassword Generator!")
-nr_letters= int(input("How many letters would you like in your password?\n")) 
-nr_symbols = int(input(f"How many symbols would you like?\n"))
-nr_numbers = int(input(f"How many numbers would you like?\n"))
 
+# Küsi kasutajalt parooli pikkust ja osade arvu
+nr_letters = int(input("How many letters would you like in your password?\n"))
+nr_symbols = int(input("How many symbols would you like?\n"))
+nr_numbers = int(input("How many numbers would you like?\n"))
 
-
-# Eazy Level ( koodi kasutamiseks eemaldage # )
-
-#password = ""
-
-#for char in range(1, nr_letters + 1):
-  #password += random.choice(letters)
-
-#for char in range(1, nr_symbols + 1):
-  # password += random.choice(symbols)
-
-#for char in range(1, nr_numbers + 1):
-  # password += random.choice(numbers)
-
-#print(password) 
-
-
-
-# Hard Level
+# Lihtsam meetod
 password_list = []
 
-for char in range(1, nr_letters + 1):
-  password_list.append(random.choice(letters))
+for _ in range(nr_letters):
+    password_list.append(random.choice(letters))
 
-for char in range(1, nr_symbols + 1):
-   password_list += random.choice(symbols)
+for _ in range(nr_symbols):
+    password_list.append(random.choice(symbols))
 
-for char in range(1, nr_numbers + 1):
-   password_list +=  random.choice(numbers)
+for _ in range(nr_numbers):
+    password_list.append(random.choice(numbers))
 
-print(password_list) 
+# Segunevad paremini
 random.shuffle(password_list)
-print(password_list)
 
-password = ""
-for char in password_list:
-   password += char
+# Lihtsam liitmine
+password = ''.join(password_list)
 
 print(f"Your password is: {password}")
